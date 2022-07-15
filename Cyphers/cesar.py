4befbb2ad78334b62333632009c76583256e0286
Kauan <jailma.kmp@gmail.com>
2022-07-15 17:07:16 -0300

    [feat] cesar cypher function init

---
+++ Cyphers/cesar.py
@@ -0,0 +1,16 @@
+def cesar (text):
+    key = 3
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    cypherAlpha = ''
+
+    for l in text.upper():
+        index = alphabet.find(l) + key
+
+        if index >= 26:
+            index = index - 26
+
+        cypherAlpha += alphabet[index]
+
+    return cypherAlpha
+
+print(cesar('wxyz'))
