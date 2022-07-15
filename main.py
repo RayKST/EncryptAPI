4f584ba15161667b0bc375ac7a420887545236fb
Kauan <jailma.kmp@gmail.com>
2022-07-15 19:36:38 -0300

    [feat] remove index endpoint and fix '/morse' return

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 from fastapi import FastAPI
 from Cyphers.cesar import Cesar
+from Cyphers.morse import Morse
 
 app = FastAPI()
 
 #Routes
 
-@app.get("/")
-def index(value = None):
-    if value == None:
-        return {
-        "Return" : "Hello World!"
-    }
-    else:
-        return {
-        "Message" : value,
-        "Return" : "Hello World!"
-    }
-
 @app.get("/cesar")
 def cypherCesar(text : str, key : int = 3):
     return {
         "Main Text": text,
-        "Encrypy Text" : Cesar(text, key)
+        "Encrypt Text" : Cesar(text, key)
+    }
+
+@app.get("/morse")
+def morseCode(text : str):
+    return {
+        "Main Text": text,
+        "Encrypt Text" : Morse(text)
     }
